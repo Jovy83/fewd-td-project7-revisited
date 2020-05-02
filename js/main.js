74e49ac601b3154e1a85f8e39a6506c159390880
Jovy83 <jovy83@live.com>
2020-05-02 20:39:36 +0800

    add traffic chart mode functionality requirement

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const $newNotificationDot = $(`#dot`);
 const $notificationsDiv = $(`#notifications`);
 const $notificationEntryDivs = $(`.notification-entry`);
 const $alertSection = $(`#alert`);
+const $trafficNav = $(`.traffic-nav`);
+const $trafficNavLinks = $(`.traffic-nav-link`);
 const $trafficChartElement = $(`#traffic-chart`);
 const $dailyChartElement = $(`#daily-chart`);
 const $platformChartElement = $(`#platform-chart`);
@@ -88,13 +90,58 @@ $alertSection.on(`click`, (event) => {
 /*              Chart JS                         */
 /* ============================================= */
 
-const trafficData = {
+const hourlyTrafficData = {
+    labels: [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `July`,
+    `Aug`, `Sep`, `Oct`, `Nov`, `Dec`],
+    datasets: [{
+        label: `Traffic`,
+        data: [50, 150, 25, 80, 200, 400, 0, 100, 20, 100,
+        600, 50],
+        backgroundColor: `rgba(226, 227, 244, 0.5)`,
+        borderColor: `rgba(115, 121, 186, 1)`,
+        borderWidth: 1,
+        pointBackgroundColor: `#fff`,
+        lineTension: 0
+    }]
+};
+
+const dailyTrafficData = {
+    labels: [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `July`,
+    `Aug`, `Sep`, `Oct`, `Nov`, `Dec`],
+    datasets: [{
+        label: `Traffic`,
+        data: [100, 150, 200, 250, 200, 225, 150, 160, 300, 350,
+        270, 225],
+        backgroundColor: `rgba(226, 227, 244, 0.5)`,
+        borderColor: `rgba(115, 121, 186, 1)`,
+        borderWidth: 1,
+        pointBackgroundColor: `#fff`,
+        lineTension: 0
+    }]
+};
+
+const weeklyTrafficData = {
     labels: [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `July`,
     `Aug`, `Sep`, `Oct`, `Nov`, `Dec`],
     datasets: [{
         label: `Traffic`,
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
         2500, 2000],
+        backgroundColor: `rgba(226, 227, 244, 0.5)`,
+        borderColor: `rgba(115, 121, 186, 1)`,
+        borderWidth: 1,
+        pointBackgroundColor: `#fff`,
+        lineTension: 0
+    }]
+};
+
+const monthlyTrafficData = {
+    labels: [`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `July`,
+    `Aug`, `Sep`, `Oct`, `Nov`, `Dec`],
+    datasets: [{
+        label: `Traffic`,
+        data: [800, 1500, 1250, 2250, 1750, 2000, 1500, 2000, 2500, 1750,
+        2750, 2350],
         backgroundColor: `rgba(226, 227, 244, 0.5)`,
         borderColor: `rgba(115, 121, 186, 1)`,
         borderWidth: 1,
@@ -122,8 +169,52 @@ const trafficOptions = {
 
 let trafficChart = new Chart($trafficChartElement, {
     type: `line`,
-    data: trafficData,
+    data: weeklyTrafficData,
     options: trafficOptions
+});
+
+const updateTrafficChart = mode => {
+    if (mode === `hourly`) {
+        trafficChart = new Chart($trafficChartElement, {
+            type: `line`,
+            data: hourlyTrafficData,
+            options: trafficOptions
+        });
+    } else if (mode === `daily`) {
+        trafficChart = new Chart($trafficChartElement, {
+            type: `line`,
+            data: dailyTrafficData,
+            options: trafficOptions
+        });
+    } else if (mode === `weekly`) {
+        trafficChart = new Chart($trafficChartElement, {
+            type: `line`,
+            data: weeklyTrafficData,
+            options: trafficOptions
+        });
+    } else {
+        trafficChart = new Chart($trafficChartElement, {
+            type: `line`,
+            data: monthlyTrafficData,
+            options: trafficOptions
+        });
+    }
+}
+
+// handle chart type change
+$trafficNav.on(`click`, `li a` , (event) => {
+
+    // need to remove selected class from all the li a
+    const clicked = event.target;
+    $trafficNavLinks.each((i, obj) => {
+        $(obj).removeClass(`selected`);
+    });
+
+    // then need to add the selected class to whatever was selected
+    $(clicked).addClass(`selected`);
+
+    // finally, need to update the chart
+    updateTrafficChart(clicked.id);
 });
 
 const dailyData = {
@@ -278,6 +369,4 @@ $(document).ready( ()=> {
 });
 
 
-//TODO: fix chart resizing -- no need to handle 
 //TODO: user search
-//TODO: traffic chart functionality
